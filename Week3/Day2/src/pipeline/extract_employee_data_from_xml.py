f236ec4310bbd66b496907f0f2f2c1fd25f41c58
Saphall <safalsakha@gmail.com>
2021-09-15 10:41:40 +0545

    ETL Day2 Assignment

---
+++ Week3/Day2/src/pipeline/extract_employee_data_from_xml.py
@@ -0,0 +1,34 @@
+from database_connection import *
+from lxml import etree
+
+
+def extract_employee_data_from_xml(filePath):
+    try:
+        con = databaseConnect('etl_day2')
+        cur = con.cursor()
+        
+        parser = etree.parse(filePath)
+        columns = ('employee_id','first_name','last_name','department_id','department_name','manager_employee_id','employee_role','salary','hire_date','terminated_date','terminated_reason','dob','fte','location')
+        for i in parser.findall('Employee'):
+        #    print(i)
+            values = [i.find(n).text for n in columns]
+            # print(p)
+    
+            sql = '''INSERT INTO raw_employee VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'''
+
+            cur.execute(sql,values)
+            con.commit()
+
+        print('[+] Extraction Successful!')
+        databaseDisconnect(con,cur)
+
+    except Exception as e:
+        print('[-] Exception Occured:',e)
+
+
+
+
+if __name__ == '__main__':
+    extract_employee_data_from_xml('../../data/employee_2021_08_01.xml')
+
+
